26fb741bba681d15a449bb3a5f31fdf2f8b31c65
Carlos Palacios <carlos_a.riascos@uao.edu.co>
2024-06-25 23:52:00 -0500

    collectable sounds added

--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -3,6 +3,13 @@ using UnityEngine;
 public class Collectable : MonoBehaviour
 {
     public static int totalCollectables = 0;
+    private AudioSource audioSource;
+    private bool isCollecting = false;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -14,8 +21,27 @@ public class Collectable : MonoBehaviour
 
     private void CollectTarget()
     {
+        if (isCollecting)
+        {
+            return;
+        }
+
+        isCollecting = true;
         totalCollectables++;
-        Destroy(gameObject);
         UIManager.Instance.UpdateCollectablesText();
+
+        if (audioSource != null)
+        {
+            audioSource.Play();
+        }
+
+        GetComponent<Renderer>().enabled = false;
+        GetComponent<Collider>().enabled = false;
+        Invoke("DeactivateObject", audioSource.clip.length);
+    }
+
+    private void DeactivateObject()
+    {
+        gameObject.SetActive(false);
     }
 }
